80317ca40ce5523294939baae09d773476df61c4
Lam Do <dothanhlam.98@gmail.com>
2023-08-01 15:29:29 +0100

    Update valid-parentheses.py

--- Stack-Queue/valid-parentheses.py
+++ Stack-Queue/valid-parentheses.py
@@ -3,9 +3,9 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         for x in s:
-            if x in ['(', '[', '{']:
+            if x in ['(', '[', '{']:  # if x == '(' or x == '[' or x == '{':
                 stack.append(x)
-            else:
+            else: # x == ')' or x == ']' or x == '}'
                 if not stack: # stack is empty
                     return False
                 last_ele = stack.pop()
@@ -13,6 +13,10 @@ class Solution:
                     return False
 
         return len(stack) == 0
+        # if len(stack) != 0:
+        #    return False
+        # else:
+        #   return True 
 
     def match(self, x, y):
         return (x == '(' and y == ')') or \
